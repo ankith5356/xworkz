15538d803df6926c646fe94f0238ab2e0d68969e
ankith5356 <116246352+ankith5356@users.noreply.github.com>
2025-03-21 11:25:45 +0530

    Instance Methods

--- ApartmentR.java
+++ ApartmentR.java
@@ -18,5 +18,63 @@ public class ApartmentR {
         second.noOfBlocks = 3;
         second.noOfFlats = 30;
         System.out.println("ID is " + second.id);
+
+        Apartment1 third = new Apartment1();
+        second.id = 203;
+        second.firstName = "Lake View";
+        second.address = "Panaji";
+        second.noOfBlocks = 3;
+        second.noOfFlats = 35;
+        System.out.println("ID is " + third.id);
+
+        Apartment1 four = new Apartment1();
+        second.id = 204;
+        second.firstName = "Pent View";
+        second.address = "Vishaka";
+        second.noOfBlocks = 30;
+        second.noOfFlats = 13;
+        System.out.println("ID is " + four.id);
+
+        Apartment1 five = new Apartment1();
+        second.id = 205;
+        second.firstName = "Casagard Amor";
+        second.address = "Mumbai";
+        second.noOfBlocks = 10;
+        second.noOfFlats = 10;
+        System.out.println("ID is " + five.id);
+
+        Apartment1 six = new Apartment1();
+        second.id = 206;
+        second.firstName = "Casagard Zaiden";
+        second.address = "Pondicherry";
+        second.noOfBlocks = 30;
+        second.noOfFlats = 13;
+        System.out.println("ID is " + six.id);
+
+        Apartment1 seven = new Apartment1();
+        second.id = 207;
+        second.firstName = "Casagrand Keatsway";
+        second.address = "Rajajinagar";
+        second.noOfBlocks = 10;
+        second.noOfFlats = 2;
+        System.out.println("ID is " + seven.id);
+
+        Apartment1 eight = new Apartment1();
+        second.id = 208;
+        second.firstName = "Casagrand Vivacity ronio";
+        second.address = "Brigade";
+        second.noOfBlocks = 30;
+        second.noOfFlats = 13;
+        System.out.println("ID is " + eight.id);
+
+        Apartment1 nine = new Apartment1();
+        second.id = 209;
+        second.firstName = "Pent ";
+        second.address = "Vishwapura";
+        second.noOfBlocks = 30;
+        second.noOfFlats = 13;
+        System.out.println("ID is " + nine.id);
+
+
     }
 }
